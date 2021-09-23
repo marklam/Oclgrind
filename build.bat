a23080d1789da02f65f60e21b924f1f9bb0051ed
Mark Lambert <marklam@spunow.co.uk>
2021-09-23 10:57:21 +0100

    Build and install, and point registry at installed copy

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
-cmake -D "LLVM_DIR=C:\tools\llvm-11.1.0-windows-amd64-msvc15-msvcrt\lib\cmake\llvm" -D "CLANG_ROOT=c:\tools\clang-11.1.0-windows-amd64-msvc15-msvcrt" 
-msbuild oclgrind.sln /p:Configuration=Release /p:Platform=x64  
+cmake -D "LLVM_DIR=C:\tools\llvm-11.1.0-windows-amd64-msvc15-msvcrt\lib\cmake\llvm" -D "CLANG_ROOT=c:\tools\clang-11.1.0-windows-amd64-msvc15-msvcrt"  -D "CMAKE_INSTALL_PREFIX=C:\Program Files\Oclgrind"
+msbuild oclgrind.sln /p:Configuration=Release /p:Platform=x64 /t:Rebuild
+REM didn't work first time ?!
+msbuild oclgrind.sln /p:Configuration=Release /p:Platform=x64
+REM (as administrator)
+msbuild /p:Configuration=Release /p:Platform=x64 INSTALL.vcxproj
+regedit /S x64only.reg
